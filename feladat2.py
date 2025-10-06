abb600a29bffcb84e1313874b68f30c928b4ece1
1Hunor4 <c2verhun@vasvari.org>
2025-10-06 09:38:09 +0200

    Szerkesztve második feladat (#1)

--- feladat2.py
+++ feladat2.py
@@ -1,3 +1,7 @@
 a = str(input("A szöveg első fele: "))
 b = str(input("A szöveg másik fele: "))
 print(f"A két szöveg összefűzve: {a+b}")
+
+a = int(input("Kérek egy számot: "))
+b = int(input("Kérek még egy számot: "))
+print("A két szám szorzata: " + a*b)
